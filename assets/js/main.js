0c21724a90afb32d89d486edef517bbee1b684ed
davide-rullo <davide.rullo01@gmail.com>
2023-08-25 15:55:21 +0200

    add
    added square script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 
 const div = document.querySelector("div.container");
+const blocco = document.createElement("div");
+blocco.className = "square";
 
 
 
@@ -7,21 +9,23 @@ for (let i = 1; i <= 100; i++) {
   
     if (i % 3 === 0 && i % 5 === 0){
         console.log("fizzbuzz");
-        const blocco = document.createElement("div");
-        blocco.className = "square";
-        blocco.append("fizzbuzz");
+        div.append("fizzbuzz");
+        div.append(blocco);
 
     } else if (i % 3 === 0) {
         console.log("fizz");
         div.append("fizz");
+        div.append(blocco);
 
     } else if (i % 5 === 0) {
         console.log("buzz");
         div.append("buzz");
+        div.append(blocco);
 
     } else {
         console.log(i);
         div.append(i);
+        div.append(blocco);
     }
 
     
