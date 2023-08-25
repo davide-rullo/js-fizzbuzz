752028c4e9cf5421eaca0710df010427ee0f9b15
davide-rullo <davide.rullo01@gmail.com>
2023-08-25 15:39:58 +0200

    add
    added script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,27 @@
 
-const blocco = document.createElement("div");
-blocco.className = "container";
-blocco.textContent = "ciao";
+const div = document.querySelector("div.container");
+
+
 
 for (let i = 1; i <= 100; i++) {
   
     if (i % 3 === 0 && i % 5 === 0){
         console.log("fizzbuzz");
+        const blocco = document.createElement("div");
+        blocco.className = "square";
+        blocco.append("fizzbuzz");
+
     } else if (i % 3 === 0) {
         console.log("fizz");
+        div.append("fizz");
+
     } else if (i % 5 === 0) {
         console.log("buzz");
+        div.append("buzz");
+
     } else {
         console.log(i);
+        div.append(i);
     }
 
     
